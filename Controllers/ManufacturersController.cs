123a6b3e19903013b012d1a5bf5d6a7705b660f5
sstrvs@outlook.com <sstrvs@outlook.com>
2022-10-22 17:14:21 +0300

    ManufacturerController now gets all data from API

--- Controllers/ManufacturersController.cs
+++ Controllers/ManufacturersController.cs
@@ -11,6 +11,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MyCRM_Online.ViewModels.Manufacturers;
+using MyCRM_Online.ViewModels.ExchangeRates;
+using Newtonsoft.Json;
+using System.Net.Http;
+using MyCRM_Online.Extensions;
 
 namespace MyCRM_Online.Controllers
 {
@@ -19,23 +23,26 @@ namespace MyCRM_Online.Controllers
     {
         private readonly DataContext dataContext;
         private readonly IMapper mapper;
+        private readonly HttpClient httpClient;
 
-        public ManufacturersController(DataContext dataContext, IMapper mapper)
+        public ManufacturersController(DataContext dataContext, IMapper mapper, IHttpClientFactory factory)
         {
             this.dataContext = dataContext;
             this.mapper = mapper;
+            this.httpClient = factory.CreateClient("apiClient");
         }
 
         public async Task<IActionResult> Index(int page = 1)
         {
-            int pageSize = 5;
+            var pageInfo = new PageInfo<ManufacturerEntity>();
 
-            IQueryable<ManufacturerEntity> source = dataContext.Manufacturers;
-            var totalCount = await source.CountAsync();
-            var entities = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
-            var manufacturers = mapper.Map<List<ManufacturerEntity>>(entities);
+            using (var response = await httpClient.GetAsync($"/api/manufacturers?page={page}"))
+            {
+                response.ThrowOnHttpError();
 
-            var pageInfo = new PageInfo<ManufacturerEntity>(totalCount, page, pageSize, manufacturers);
+                var apiResponse = await response.Content.ReadAsStringAsync();
+                pageInfo = JsonConvert.DeserializeObject<PageInfo<ManufacturerEntity>>(apiResponse);
+            }
 
             return View(pageInfo);
         }
@@ -46,77 +53,77 @@ namespace MyCRM_Online.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromForm] ManufacturerCreateViewModel manufacturer)
+        public async Task<IActionResult> Create([FromForm] ManufacturerCreateViewModel manufacturer)
         {
             if (!ModelState.IsValid)
             {
                 return View(manufacturer);                
             }
 
-            var newManufacturer = mapper.Map<ManufacturerEntity>(manufacturer);
-            dataContext.Manufacturers.Add(newManufacturer);
-            dataContext.SaveChanges();
+            var serializedManufacturer = JsonConvert.SerializeObject(manufacturer, Formatting.Indented);
+            var httpContent = new StringContent(serializedManufacturer, Encoding.UTF8, "application/json");
+
+            using (var response = await httpClient.PostAsync($"/api/manufacturers", httpContent))
+            {
+                response.ThrowOnHttpError();
+            }
 
             return RedirectToAction("Index");
         }
 
         [HttpGet]
-        public IActionResult Edit([FromRoute] int? id)
+        public async Task<IActionResult> Edit([FromRoute] int? id)
         {
-            if (id == null || id == 0)
-            {
+            if (id == null || id < 1) {
                 return NotFound();
             }
 
-            var entity = dataContext.Manufacturers.Find(id);
+            ManufacturerEditViewModel manufacturer;
 
-            if (entity == null)
+            using (var response = await httpClient.GetAsync($"/api/manufacturers/{id}"))
             {
-                return NotFound();
-            }
+                response.ThrowOnHttpError();
 
-            var manufacturer = mapper.Map<ManufacturerEditViewModel>(entity);
+                var apiResponse = await response.Content.ReadAsStringAsync();
+                manufacturer = JsonConvert.DeserializeObject<ManufacturerEditViewModel>(apiResponse);
+            }
 
             return View(manufacturer);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit([FromRoute] int? id, ManufacturerEditViewModel manufacturer)
+        public async Task<IActionResult> Edit([FromRoute] int? id, ManufacturerEditViewModel manufacturer)
         {
-            if (manufacturer.Id != id)
-            {
+            if (manufacturer.Id != id) {
                 return BadRequest();
             }
 
-            if (!ModelState.IsValid)
-            {
+            if (!ModelState.IsValid) {
                 return View(manufacturer);
             }
 
-            var entity = dataContext.Manufacturers.Find(id);
+            var serializedManufacturer = JsonConvert.SerializeObject(manufacturer, Formatting.Indented);
+            var httpContent = new StringContent(serializedManufacturer, Encoding.UTF8, "application/json");
 
-            if (entity == null)
+            using (var response = await httpClient.PutAsync($"/api/manufacturers", httpContent))
             {
-                return NotFound();
+                response.ThrowOnHttpError();
             }
-
-            mapper.Map(manufacturer, entity);
-            dataContext.SaveChanges();
 
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult Delete([FromForm] int? id)
+        public async Task<IActionResult> Delete([FromForm] int? id)
         {
-            if (id == null || id == 0)
-            {
+            if (id == null || id < 1) {
                 return NotFound();
             }
 
-            dataContext.Manufacturers.Remove(new ManufacturerEntity() { Id = id });
-            dataContext.SaveChanges();
+            using (var response = await httpClient.DeleteAsync($"/api/manufacturers/{id}")) {
+                response.ThrowOnHttpError();
+            }
 
             return RedirectToAction("Index");
         }
